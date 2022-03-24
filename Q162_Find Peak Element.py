63cd111fb938ce7be330a5b8bd44571956e2d2f7
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-03-24 10:44:13 -0400

    Update Q162_Find Peak Element.py

--- Q162_Find Peak Element.py
+++ Q162_Find Peak Element.py
@@ -4,3 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        left,right = 0, len(nums)-1
+        if(len(nums) == 1):
+            return right
+        while(left <= right):
+            ans = -1
+            mid = (right + left) // 2
+            if(left == right):
+                return mid
+            if (nums[mid] > nums[mid+1] and nums[mid-1] < nums[mid]):
+                return mid
+            elif(nums[mid] < nums[mid+1]):
+                left += 1 
+            else:
+                right -= 1
+                
+        
+        return ans
