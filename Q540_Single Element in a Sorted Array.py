e5d2f96510f8f8bbe3654259cbde00fea7a4a436
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-03-24 16:35:58 -0400

    Update Q540_Single Element in a Sorted Array.py

--- Q540_Single Element in a Sorted Array.py
+++ Q540_Single Element in a Sorted Array.py
@@ -5,10 +5,15 @@ class Solution(object):
             return nums[right]
         while left <= right:
             mid = (left+right)//2
-            if (mid!=len(nums)-1 and nums[mid]==nums[mid+1]): #comparing 1st appearence with next element
-                mid += 1;#setting mid to 2nd appearence 
+            #we want to know the single element is in left part or right part.
+            #comparing 1st appearence with next element
+            #we just make this pair in left
+            if (mid!=len(nums)-1 and nums[mid]==nums[mid+1]): 
+                mid += 1;#setting mid to 2nd appearence
+            #if in this case, the single part is in right part.
             if mid%2!=0:#searching right side of the array
                 left = mid+1;
+            #if in this case, the single part is in left part.
             else :
                 if mid==0 or nums[mid-1]!=nums[mid]:# got the mid
                     return nums[mid]
