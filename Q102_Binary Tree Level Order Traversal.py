a4bc8bf31d7c5d7ca18dafec6e7cdfcec68aa533
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-03-26 10:17:11 -0400

    Update Q102_Binary Tree Level Order Traversal.py

--- Q102_Binary Tree Level Order Traversal.py
+++ Q102_Binary Tree Level Order Traversal.py
@@ -5,8 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    
+    def breadth_first_search(self, root, result, level):
+		if root==None:
+			return root
+		if len(result) == level:
+			result.append([])
+		result[level].append(root.val)
+		self.breadth_first_search(root.left,  result, level + 1)
+		self.breadth_first_search(root.right, result, level + 1)
+        
     def levelOrder(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
-        
+        """
+        if root is None:
+			return root
+        result = []
+        self.breadth_first_search(root, result, 0)
+        return result
