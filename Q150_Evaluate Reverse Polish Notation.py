d6593a6a0d92572bccc9b44ef4b1eab72aac3da8
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-04-01 17:10:21 -0400

    Update Q150_Evaluate Reverse Polish Notation.py

--- Q150_Evaluate Reverse Polish Notation.py
+++ Q150_Evaluate Reverse Polish Notation.py
@@ -24,3 +24,46 @@ class Solution(object):
                 elif token == '/':
                     stack.append(int(first/second))
         return(stack[0])
+    
+    ################################################################
+    class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        op = ('+', '-', '*', '/')
+        res =[]
+        for i in tokens:
+            if i not in op:
+                res.append(i)
+            elif i == '+':
+                x = int(res[-2])
+                y = int(res[-1])
+                res.pop(-1)
+                res[-1] = x + y
+            elif i == '-':
+                x = int(res[-2])
+                y = int(res[-1])
+                res.pop(-1)
+                res[-1] = x - y
+            elif i == '*':
+                x = int(res[-2])
+                y = int(res[-1])
+                res.pop(-1)
+                res[-1] = x * y
+            elif i == '/':
+                state = 1
+                x = int(res[-2])
+                y = int(res[-1])
+                res.pop(-1)
+                if x < 0:
+                    state = 1 - state
+                if y < 0:
+                    state = 1 - state
+                if state:
+                    res[-1] = int(x / y)
+                else:
+                    res[-1] = int(abs(x) / abs(y)) *(-1)
+                
+        return res[0]
