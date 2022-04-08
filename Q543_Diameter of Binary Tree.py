f37149349bdc2fbe425e33cd3ec6ce67ab2fc513
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-04-07 22:20:43 -0400

    Update Q543_Diameter of Binary Tree.py

--- Q543_Diameter of Binary Tree.py
+++ Q543_Diameter of Binary Tree.py
@@ -19,10 +19,12 @@ class Solution(object):
             #when it in the leaf.
             if not root:
                 return 0
-            #get the left adn right is exist or not
+            #get the left and right is exist or not
             left = dfs(root.left)
             right = dfs(root.right)
+            #compare the left + right and self.max_diameter. then decide this node will in rote will not.
             self.max_diameter = max(left + right,self.max_diameter)
+            #return the deep children tree
             return 1 + max(left,right)
     
         res = dfs(root)
