95d97a8e16a849ccf4c84ee3f166124111838321
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-04-08 23:06:10 -0400

    Update Q101_Symmetric Tree.py

--- Q101_Symmetric Tree.py
+++ Q101_Symmetric Tree.py
@@ -13,10 +13,13 @@ class Solution(object):
         if root is None:
             return False
         def compareSub(left_child, right_child):
+            #If both is None. return true
             if left_child == None and right_child == None:
                 return True
+            #If one of is None just return false.
             if left_child == None or right_child == None:
                 return False
+            #if two child value is not equal, return false.
             if left_child.val != right_child.val:
                 return False
             return compareSub(left_child.left, right_child.right) and compareSub(left_child.right, right_child.left)
