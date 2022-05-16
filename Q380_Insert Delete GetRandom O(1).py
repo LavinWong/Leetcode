495f00a52a173f9f772555d02bea7d9391979a3e
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2022-05-16 00:38:13 -0400

    Create Q380_Insert Delete GetRandom O(1).py

---
+++ Q380_Insert Delete GetRandom O(1).py
@@ -0,0 +1,50 @@
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.list = []
+        self.len = 0
+        
+
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        instate = False
+        if val not in self.list:
+            instate = True
+            self.list.append(val)
+            self.len += 1
+        return instate
+        
+
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        rmstate = False
+        if val in self.list:
+            self.list.remove(val)
+            rmstate = True
+            self.len -= 1
+        return rmstate
+        
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        if self.len < 1:
+            return False
+        else:
+            n = random.randint(0,self.len-1)
+            return self.list[n]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
